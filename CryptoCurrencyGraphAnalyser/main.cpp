a13af344fcfdc6154cf9051ab80333fc481f100e
Henry Schimke <henry@boots.tree>
2014-04-19 19:13:35 -0500

    Initial Commit

---
+++ CryptoCurrencyGraphAnalyser/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CryptoCurrencyGraphAnalyser
+//
+//  Created by Henry Schimke on 4/19/14.
+//  Copyright (c) 2014 Henry Schimke. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
